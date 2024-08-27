9015966633045109a7d195a63a5099d71382d34b
3psilon0 <bestofluck110@gmail.com>
2024-08-27 21:19:51 +0500

    Added functionality for toggling gridlines, changing color and clearing grid

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 const gridContainer = document.getElementById("grid-container");
 const buttonContainer = document.getElementById("button-container");
 const gridSlider = document.getElementById("grid-slider");
+
 let penColor = "black";
+let bgColor = "white";
 
 const setGridPixelColor = (grid) => {
     grid.target.style.backgroundColor = penColor;
 };
 let currentFunction = setGridPixelColor;
 
-
-
 const gridSliderValue = document.createElement("p");
 gridSliderValue.style.cssText = "text-align:center;margin-top:-5px;";
 gridSlider.after(gridSliderValue);
 
-gridSlider.addEventListener("input", function (){
+gridSlider.addEventListener("input", function () {
     gridSliderValue.innerText = `${this.value} x ${this.value}`;
     gridContainer.textContent = "";
-    for(let i = 0; i < this.value; i++){
+    for (let i = 0; i < this.value; i++) {
         let column = document.createElement("div");
-        column.style.cssText = "display:flex;flex-direction:column;"
-        for(let j = 0; j < this.value; j++){
+        column.style.cssText = "display:flex;flex-direction:column;";
+        for (let j = 0; j < this.value; j++) {
             let pixel = document.createElement("div");
             pixel.classList.add("pixel-border");
             pixel.style.boxSizing = "border-box";
-            pixel.style.width = `${550/this.value}px`;
-            pixel.style.height = `${550/this.value}px`;
+            pixel.style.width = `${550 / this.value}px`;
+            pixel.style.height = `${550 / this.value}px`;
             column.appendChild(pixel);
         }
         gridContainer.append(column);
@@ -35,12 +35,36 @@ gridSlider.addEventListener("input", function (){
 gridContainer.addEventListener("mousedown", (e) => {
     gridContainer.addEventListener("mousedown", currentFunction);
     gridContainer.addEventListener("mousemove", currentFunction);
-    
 });
 
 gridContainer.addEventListener("mouseup", () => {
     gridContainer.removeEventListener("mousemove", currentFunction);
 });
 
-const onLoad = new Event('input');
+buttonContainer.addEventListener("click", (e) => {
+    for (let i = 0; i < gridContainer.children.length; i++) {
+        for (let j = 0; j < gridContainer.children.length; j++) {
+            let pixel = gridContainer.children[i].children[j];
+            switch (e.target.id) {
+                case "clear-grid-btn":
+                    pixel.style.backgroundColor = bgColor;
+                    break;
+                case "toggle-lines-btn":
+                    pixel.classList.toggle("pixel-border");
+                    break;
+                default:
+                    return;
+            }
+        }
+    }
+});
+buttonContainer.addEventListener("input", (e) => {
+    if (e.target.id === "color-pick-btn") {
+        penColor = e.target.value;
+    } else {
+        return;
+    }
+});
+
+const onLoad = new Event("input");
 gridSlider.dispatchEvent(onLoad);
